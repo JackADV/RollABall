ff6a1b3e848381894a692e5780730817bc9848f7
Jack <47583484+JackADV@users.noreply.github.com>
2019-08-07 13:29:36 +1000

    Initial commit

---
+++ Assets/Tests/TestSuite.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.TestTools;
+using NUnit.Framework;
+
+public class TestSuite
+{
+    private GameObject game;
+    private GameManager gameManager;
+    private Player player;
+    [SetUp]
+    public void Setup()
+    {
+        // Load and spawn Game prefab
+        GameObject prefab = Resources.Load<GameObject>("Prefabs/Game");
+        game = Object.Instantiate(prefab);
+        // Get GameManager
+        gameManager = GameManager.Instance;
+        player = game.GetComponentInChildren<Player>();
+
+    }
+
+    [UnityTest]
+    public IEnumerator GamePrefabLoaded()
+    {
+        yield return new WaitForEndOfFrame();
+
+        // Game object should exist at this point in time
+        Assert.NotNull(game);
+    }
+
+    [UnityTest]
+    public IEnumerator PlayerExists()
+    {
+        yield return new WaitForEndOfFrame();
+        Assert.NotNull(player, "No player dawg");
+    }
+    [TearDown]
+    public void Teardown()
+    {
+        Object.Destroy(game);
+    }
+}
